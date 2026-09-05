5040bd5d5b52e84519aa665af2f3df9ff401ab48
alicehocane <hereyouth@gmail.com>
2026-09-05 16:22:17 +0500

    address addedd

--- api/webhook.js
+++ api/webhook.js
@@ -2,8 +2,22 @@ const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL || "https://discord.
 
 // 1. YOUR LIST OF TRACKED WALLETS
 const TRACKED_WALLETS = new Set([
-    "GThUX1Atko4tqhN2NaiTazWSeFWMuiUvfFnyJyUghFMJ",
-    // Add more Solana wallet addresses here
+    "9oKGw6n6tjGC7mGuS4PLRYx1xSMt2pAzFdCibUjdaX2m",
+    "3VMW45SQTeSxFrSozjBbx49qqQ2z6HKUezvakAPwHwhz",
+    "GkVZ6BRP3nd8LYwCAdniVdTS2R8zniRbkN5tZhhvLBtY",
+    "835GEJgjDt4B4LzC33vYpt6dQXUWJqkkVnWVnGP3WUKN",
+    "7UDarSsvMu64SDyL5dayANca8za87tbyeuAdGjMKWf16",
+    "7iG1nhuNPXjUq2D2LJXywBE1zAXQQFzDFvf7hKNjZf26",
+    "EHg5YkU2SZBTvuT87rUsvxArGp3HLeye1fXaSDfuMyaf",
+    "5cQM6QHmdLPS8AEKA8KtszFmC8Rq4YrDj9WpWDtXTqSy",
+    "BgpLrxjCPFrvqEUQNrMxhP7ZmDNbjwcaX8fzy3ZuYvzT",
+    "BQdbTPv9iuPjU6swSVJEsdv3hutS1UxFstTpx4KRVZEm",
+    "3Qchg1ipMKRoqDktu27yNMvcPe3xTHD9q7uJe6Xtbua2",
+    "4w8St7tNUNqgAXvXd4BjhkByV1w9vagp2yjUyrnoU4ZS",
+    "Ggnm2KVizsXedUrBJqjXtg8ztgCw3C5P6swVfSiFQ2SH",
+    "8eGqytw6HWhykdBoA9gNWZv7t7vYr6X8KeoDABU1731y",
+    "4bUNoVcQdCwUnETSHxW9bZrzinx6MhzxL7Bd6bvqYgVh",
+    "3bwkvwoYnyC9GMVFn2EWeAJ2YptCnZDWdc284SdWD2gd"
 ]);
 
 // Persistent global cache for Vercel serverless instances
